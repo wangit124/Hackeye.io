ca4af1f20603981fd633726978c6d49d71d9045a
howard wang <luhao.wang2016@gmail.com>
2019-04-13 20:14:41 -0700

    established RESTful get protocols for all project pages

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 // Fill in hackaday API data
-var apiData = {};
+global.apiData = {};
 
 apiData.clientId = 'IqOKhUebqhieIOciR2mf2WJT90ZLkcVJ4OEIcdwgMlZEcTw5';
 apiData.clientSecret = 'Ayc2OnrtVRXV2OQtP91EEHeB8ZDAeN3J6RAANxTFrU5MVgZL';
 apiData.userKey = 'QlhAEh7ym6so8Puk';
 
-//API URLs:
+// API URLs:
 apiData.apiKey = '?api_key=' + apiData.userKey;
 apiData.apiUrl = 'https://api.hackaday.io/v1';
 apiData.apiAuthUrl = 'https://api.hackaday.io/v1/me' + apiData.apiKey;
 apiData.oAuthRedirect = 'https://hackaday.io/authorize?client_id=' + apiData.clientId + '&response_type=code';
 apiData.createTokenUrl = function (code) {
     return ('https://auth.hackaday.io/access_token?' +
-    'client_id=' + this.clientId +
-    '&client_secret=' + this.clientSecret +
-    '&code=' + code +
-    '&grant_type=authorization_code');
+        'client_id=' + this.clientId +
+        '&client_secret=' + this.clientSecret +
+        '&code=' + code +
+        '&grant_type=authorization_code');
 };
 
-// Load express, bodyparse, modules
+// Load express, bodyparse, debug and request modules
+const debug = require('debug')('http')
+const request = require('request');
 const express = require('express');
-const bodyParser = require('body-parser');  
+const bodyParser = require('body-parser');
 const app = express();
 
 // Enable EJS
@@ -28,13 +30,13 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 // Include static files
-app.use(bodyParser.urlencoded({ extended: true }));                              
-app.use(express.static('./public')); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('./public'));
 
 // Open on port 8000
 const port = 8000;
-app.listen(port, ()=> {
-    console.log('Running on localhost: '+port+'...');
+app.listen(port, () => {
+    console.log('Running on localhost: ' + port + '...');
 });
 
 // Render ejs with simple message
@@ -42,6 +44,48 @@ app.get('/', (req, res) => {
     res.render('index', {
         message: "Night Watch"
     });
+});
+
+// Render projects landing page, AKA pg 1
+app.get('/projects', (req, res) => {
+    // construct url to get certain project page
+    var pgNum = 1;
+    var url = global.apiData.apiUrl + '/projects' + apiData.apiKey + '&per_page=50' + '&page=' + pgNum;
+
+    // Make api call
+    request.get(url, (error, response, body) => {
+        // If successful, render JSON data
+        if (!error && response.statusCode === 200) {
+            res.render('projects', {
+                projects: body
+            });
+        } else {
+            console.log('\nError: ', error, '\nResponse body: ', body);
+            res.render(body);
+        }
+    });
+});
+
+// Render nth projects page 
+app.get('/projects/:pg', (req, res) => {
+    // construct url to get certain project page
+    var pgNum = req.params.pg;
+    var url = global.apiData.apiUrl + '/projects' + apiData.apiKey + '&per_page=20' + '&page=' + pgNum;
+    debug(url);
+
+    // Make api call
+    request.get(url, (error, response, body) => {
+        // If successful, render JSON data
+        if (!error && response.statusCode === 200) {
+            res.render('projects', {
+                projects: body
+            });
+        } else {
+            console.log('\nError: ', error, '\nResponse body: ', body);
+            res.render(body);
+        }
+    });
+
 });
 
 
--- tests/demotest.js
+++ tests/demotest.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'Demo test Google' : function (browser) {
+  'Demo test Google': function (browser) {
     browser
       .url('localhost:8000')
       .waitForElementVisible('body')
